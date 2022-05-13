472184d20ad583507258590430235564a3dd5ca2
Joice Rodrigues <joice2k19rodrigues@gmail.com>
2022-05-13 15:39:20 -0300

    atualização de index e cadastro

--- cadastro.php
+++ cadastro.php
@@ -12,11 +12,19 @@ require_once('header.php')
 <div class="container">
     <form action="cadastro2.php" method="post">
         <div class="mb-3">
-        <label for="nome" class="form-label"> Nome:</label>
-        <input class="form-label" type="text" id="nome" name="nome" placeholder="Digite seu nome"><br><br>
-        <label for="ra" class="form-label">ra:</label>
-        <input class="form-label" type="text" id="ra" name="ra" maxlength=7 placeholder="Digite seu RA"><br><br>
-        <input class="btn btn-primary" type="submit" value="Enviar">
+            <div class="row">
+                <div class="col">
+                    <br><br>
+                    <label for="nome" class="form-label"> Nome:</label>
+                    <input class="form-control" type="text" id="nome" name="nome" placeholder="Digite seu nome"><br><br>
+                    <label for="ra" class="form-label">RA:</label>
+                    <input class="form-control" type="text" id="ra" name="ra" maxlength=7 placeholder="Digite seu RA"><br><br>
+                    <input class="btn btn-primary" type="submit" value="Enviar">
+                </div>
+                <div class="col">
+                    <img src="images/cad.png" height="550px" widht="550px">
+                </div>
+            </div>
         </div>
     </form>
 </div>
